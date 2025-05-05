402bfabef2de8f72a32122caaf1f32cb4006e6ed
A-Aron <timelordaaron@gmail.com>
2025-05-05 17:40:35 -0500

    Filter Selection UI
    Created a basic UI for handling filter selection. Began working on the backend logic for handling filter selection from the user.

--- app/src/main/java/com/example/pokemonshinytracker/MainActivity.kt
+++ app/src/main/java/com/example/pokemonshinytracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pokemonshinytracker
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
@@ -7,8 +8,11 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
@@ -16,13 +20,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
 
-    // lateinit UI declarations
+    // lateinit UI declarations: main UI
     private lateinit var newHuntBtn: Button                     // new hunt button
     private lateinit var filterBtn: Button                      // filter button
-    private lateinit var clearFiltersBtn: Button                // clear filters button
+    private lateinit var mainClearFiltersBtn: Button            // clear filters button (main page)
     private lateinit var expandAllCheckbox: CheckBox            // expand all checkbox
     private lateinit var shinyHuntRecyclerView: RecyclerView    // shiny hunt recycler view
     private lateinit var sortBtn: Button                        // floating sort button
@@ -34,6 +39,30 @@ class MainActivity : ComponentActivity() {
     private lateinit var sortFinishDateBtn: Button              // sort by finish date button
     private lateinit var sortNameBtn: Button                    // sort by name button
     private lateinit var sortGenerationBtn: Button              // sort by generation button
+
+    // lateinit UI declarations: filter selection UI
+    private lateinit var filterClearFiltersBtn: Button          // clear filters button (filter selection dialog)
+    private lateinit var editPokemonBtn: Button                 // edit pokemon button
+    private lateinit var pokemonTxt: TextView                   // selected pokemon text
+    private lateinit var editOriginGamesBtn: Button             // edit origin games button
+    private lateinit var originGamesTxt: TextView               // selected origin games text
+    private lateinit var completionStatusRadioGrp: RadioGroup   // completion status radio group
+    private lateinit var inProgressRadioBtn: RadioButton        // in progress radio button (for completion status)
+    private lateinit var completedRadioBtn: RadioButton         // completed radio button (for completion status)
+    private lateinit var bothRadioBtn: RadioButton              // both radio button (for completion status)
+    private lateinit var currentGamesLayout: LinearLayout       // current games layout
+    private lateinit var editCurrentGamesBtn: Button            // edit current games button
+    private lateinit var currentGamesTxt: TextView              // selected current games text
+    private lateinit var method: EditText                       // method text field
+    private lateinit var startDateFromBtn: Button               // start date from button
+    private lateinit var startDateToBtn: Button                 // start date to button
+    private lateinit var finishDateFromBtn: Button              // finish date from button
+    private lateinit var finishDateToBtn: Button                // finish date to button
+    private lateinit var counterLo: EditText                    // counter (low bound) text field
+    private lateinit var counterHi: EditText                    // counter (high bound) text field
+    private lateinit var phaseLo: EditText                      // phase (low bound) text field
+    private lateinit var phaseHi: EditText                      // phase (high bound) text field
+    private lateinit var confirmFiltersBtn: Button              // confirm filters button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("MainActivity", "onCreate() started")
@@ -54,6 +83,21 @@ class MainActivity : ComponentActivity() {
         var currentSortMethodIndex = 0          // default sort method is DEFAULT
         var currentSortOrders =
             arrayOf(SortOrder.DESC, SortOrder.DESC, SortOrder.DESC, SortOrder.ASC, SortOrder.ASC)
+
+        // initialize variables for tracking the selected filters
+        var selectedPokemonForms: Set<Int> = emptySet()
+        var selectedOriginGames: Set<Int> = emptySet()
+        var selectedCompletionStatus: CompletionStatus? = null
+        var selectedCurrentGames: Set<Int> = emptySet()
+        var enteredMethod = ""
+        var selectedStartDateFrom = ""
+        var selectedStartDateTo = ""
+        var selectedFinishDateFrom = ""
+        var selectedFinishDateTo = ""
+        var enteredCounterLo = ""
+        var enteredCounterHi = ""
+        var enteredPhaseLo = ""
+        var enteredPhaseHi = ""
 
         // retrieve relevant data from database
         val pokemonList = db.getPokemon()   // list of all pokemon
@@ -78,7 +122,7 @@ class MainActivity : ComponentActivity() {
         shinyHuntRecyclerView = findViewById(R.id.shiny_hunts_recycler_view)  // recycler view that displays the user's saved hunts
         newHuntBtn = findViewById(R.id.new_hunt_button)                                         // new hunt button
         filterBtn = findViewById(R.id.filter_button)                                            // filter button
-        clearFiltersBtn = findViewById(R.id.clear_filters_button)                               // clear filters button
+        mainClearFiltersBtn = findViewById(R.id.clear_filters_button)                           // clear filters button (main page)
         expandAllCheckbox = findViewById(R.id.expand_all_checkbox)                              // expand all checkbox
         sortBtn = findViewById(R.id.sort_button)                                                // floating sort button
         sortMenu = findViewById(R.id.sort_menu)                                                 // sort menu
@@ -161,13 +205,195 @@ class MainActivity : ComponentActivity() {
             this.startActivity(intent)
         }
 
+        /* Helper functions for setting UI in the filter selection dialog */
+        // selected pokemon
+        fun setPokemon() {
+            if (selectedPokemonForms.size == 1) {
+                pokemonTxt.text = "1 Pokemon form selected."
+            } else {
+                pokemonTxt.text = "${selectedPokemonForms.size} Pokemon forms selected."
+            }
+        }
+        // selected origin games
+        fun setOriginGames() {
+            if (selectedOriginGames.size == 1) {
+                originGamesTxt.text = "1 game selected."
+            } else {
+                originGamesTxt.text = "${selectedOriginGames.size} games selected."
+            }
+        }
+        // selected completion status
+        fun setCompletionStatus() {
+            when (selectedCompletionStatus) {
+                CompletionStatus.IN_PROGRESS -> completionStatusRadioGrp.check(R.id.in_progress_radio_button)
+                CompletionStatus.COMPLETE -> completionStatusRadioGrp.check(R.id.completed_radio_button)
+                CompletionStatus.BOTH -> completionStatusRadioGrp.check(R.id.both_radio_button)
+                else -> {}  // do nothing
+            }
+        }
+        // selected current games
+        fun setCurrentGames() {
+            if (selectedCompletionStatus == null || selectedCompletionStatus == CompletionStatus.IN_PROGRESS) {
+                currentGamesLayout.visibility = View.GONE
+            } else {
+                currentGamesLayout.visibility = View.VISIBLE
+                if (selectedCurrentGames.size == 1) {
+                    currentGamesTxt.text = "1 game selected."
+                } else {
+                    currentGamesTxt.text = "${selectedCurrentGames.size} games selected."
+                }
+            }
+        }
+        // entered method
+        fun setMethod() { method.setText(enteredMethod) }
+        // start date from
+        fun setStartDateFrom() { startDateFromBtn.text = selectedStartDateFrom }
+        // start date to
+        fun setStartDateTo() { startDateToBtn.text = selectedStartDateTo }
+        // finish date from
+        fun setFinishDateFrom() { finishDateFromBtn.text = selectedFinishDateFrom }
+        // finish date to
+        fun setFinishDateTo() { finishDateToBtn.text = selectedFinishDateTo }
+        // counter low
+        fun setCounterLo() { counterLo.setText(enteredCounterLo) }
+        // counter high
+        fun setCounterHi() { counterHi.setText(enteredCounterHi) }
+        // phase low
+        fun setPhaseLo() { phaseLo.setText(enteredPhaseLo) }
+        // phase high
+        fun setPhaseHi() { phaseHi.setText(enteredPhaseHi) }
+
         // on click listener for the filter button
         filterBtn.setOnClickListener {
-            // TODO: Implement filter button logic
+            Log.d("MainActivity", "Filter button clicked. Opening filters dialog")
+
+            val filterDialog = layoutInflater.inflate(R.layout.filter_selection, null)
+
+            // access all the UI elements
+            filterClearFiltersBtn = filterDialog.findViewById(R.id.clear_filters_button)    // clear filters button (filter selection dialog)
+            editPokemonBtn = filterDialog.findViewById(R.id.edit_pokemon_button)            // edit pokemon button
+            pokemonTxt = filterDialog.findViewById(R.id.pokemon_text)                       // selected pokemon text
+            editOriginGamesBtn = filterDialog.findViewById(R.id.edit_origin_games_button)   // edit origin games button
+            originGamesTxt = filterDialog.findViewById(R.id.origin_games_text)              // selected origin games text
+            completionStatusRadioGrp = filterDialog.findViewById(R.id.completion_statuses_radio_group)  // completion status radio group
+            inProgressRadioBtn = filterDialog.findViewById(R.id.in_progress_radio_button)   // in progress radio button (for completion status)
+            completedRadioBtn = filterDialog.findViewById(R.id.completed_radio_button)      // completed radio button (for completion status)
+            bothRadioBtn = filterDialog.findViewById(R.id.both_radio_button)                // both radio button (for completion status)
+            currentGamesLayout = filterDialog.findViewById(R.id.current_games_layout)       // current games layout
+            editCurrentGamesBtn = filterDialog.findViewById(R.id.edit_current_games_button) // edit current games button
+            currentGamesTxt = filterDialog.findViewById(R.id.current_games_text)            // selected current games text
+            method = filterDialog.findViewById(R.id.method)                                 // method text field
+            startDateFromBtn = filterDialog.findViewById(R.id.start_date_from_button)       // start date from button
+            startDateToBtn = filterDialog.findViewById(R.id.start_date_to_button)           // start date to button
+            finishDateFromBtn = filterDialog.findViewById(R.id.finish_date_from_button)     // finish date from button
+            finishDateToBtn = filterDialog.findViewById(R.id.finish_date_to_button)         // finish date to button
+            counterLo = filterDialog.findViewById(R.id.counter_lo)                          // counter (low bound) text field
+            counterHi = filterDialog.findViewById(R.id.counter_hi)                          // counter (high bound) text field
+            phaseLo = filterDialog.findViewById(R.id.phase_lo)                              // phase (low bound) text field
+            phaseHi = filterDialog.findViewById(R.id.phase_hi)                              // phase (high bound) text field
+            confirmFiltersBtn = filterDialog.findViewById(R.id.confirm_filters_button)      // confirm filters button
+
+            // initialize the UI based on the currently selected filters
+            setPokemon()
+            setOriginGames()
+            setCompletionStatus()
+            setCurrentGames()
+            setMethod()
+            setStartDateFrom()
+            setStartDateTo()
+            setFinishDateFrom()
+            setFinishDateTo()
+            setCounterLo()
+            setCounterHi()
+            setPhaseLo()
+            setPhaseHi()
+
+            // display the dialog
+            AlertDialog.Builder(this)
+                .setTitle("Filters")
+                .setView(filterDialog)
+                .setPositiveButton("Close") { dialog, _ -> dialog.dismiss() }
+                .show()
+
+            // on click listener for the clear filters button (filter selection dialog)
+            filterClearFiltersBtn.setOnClickListener {
+                // TODO: Implement clear filters button logic
+                Log.d("MainActivity", "Clear Filters button clicked in the filter selection dialog")
+            }
+
+            // on click listener for the edit pokemon button
+            editPokemonBtn.setOnClickListener {
+                // TODO: Implement edit pokemon button logic
+                Log.d("MainActivity", "Edit Pokemon button clicked in the filter selection dialog")
+            }
+
+            // on click listener for the edit origin games button
+            editOriginGamesBtn.setOnClickListener {
+                // TODO: Implement edit origin games button logic
+                Log.d("MainActivity", "Edit Origin Games button clicked in the filter selection dialog")
+            }
+
+            // on click listener for the in progress radio button
+            inProgressRadioBtn.setOnClickListener {
+                Log.d("MainActivity", "In Progress radio button clicked in the filter selection dialog")
+                selectedCompletionStatus = CompletionStatus.IN_PROGRESS
+                setCurrentGames()
+            }
+
+            // on click listener for the completed radio button
+            completedRadioBtn.setOnClickListener {
+                Log.d("MainActivity", "Completed radio button clicked in the filter selection dialog")
+                selectedCompletionStatus = CompletionStatus.COMPLETE
+                setCurrentGames()
+            }
+
+            // on click listener for the both radio button
+            bothRadioBtn.setOnClickListener {
+                Log.d("MainActivity", "Both radio button clicked in the filter selection dialog")
+                selectedCompletionStatus = CompletionStatus.BOTH
+                setCurrentGames()
+            }
+
+            // on click listener for the edit current games button
+            editCurrentGamesBtn.setOnClickListener {
+                // TODO: Implement edit current games button logic
+                Log.d("MainActivity", "Edit Current Games button clicked in the filter selection dialog")
+            }
+
+            // on click listener for the start date from button
+            startDateFromBtn.setOnClickListener {
+                // TODO: Implement start date from button logic
+                Log.d("MainActivity", "Start Date From button clicked in the filter selection dialog")
+            }
+
+            // on click listener for the start date to button
+            startDateToBtn.setOnClickListener {
+                // TODO: Implement start date to button logic
+                Log.d("MainActivity", "Start Date To button clicked in the filter selection dialog")
+            }
+
+            // on click listener for the finish date from button
+            finishDateFromBtn.setOnClickListener {
+                // TODO: Implement finish date from button logic
+                Log.d("MainActivity", "Finish Date From button clicked in the filter selection dialog")
+            }
+
+            // on click listener for the finish date to button
+            finishDateToBtn.setOnClickListener {
+                // TODO: Implement finish date to button logic
+                Log.d("MainActivity", "Finish Date To button clicked in the filter selection dialog")
+            }
+
+            // on click listener for the confirm filters button
+            confirmFiltersBtn.setOnClickListener {
+                // TODO: Implement the confirm filters button logic
+                Log.d("MainActivity", "Confirm Filters button clicked in the filter selection dialog")
+            }
+
         }
 
-        // on click listener for the clear filters button
-        clearFiltersBtn.setOnClickListener {
+        // on click listener for the clear filters button (main page)
+        mainClearFiltersBtn.setOnClickListener {
             // TODO: Implement clear filters button logic
         }
 
